c8d95a5d10a36360cd637d603b8cd248bd10b75e
IanR-MOJ <84918564+IanR-MOJ@users.noreply.github.com>
2022-02-24 12:44:52 +0000

    Change LB Rule form SrcIPandProt to SrcIP

--- terraform/modules/cloud-agent/load_balancer.tf
+++ terraform/modules/cloud-agent/load_balancer.tf
@@ -41,7 +41,7 @@ resource "azurerm_lb_rule" "loadbalancing_rule" {
   frontend_ip_configuration_name = "fe-${each.key}"
   probe_id                       = azurerm_lb_probe.health_probe[each.key].id
   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.backend_pool[each.key].id]
-  load_distribution              = "SourceIPProtocol"
+  load_distribution              = "SourceIP"
 }
 
 
