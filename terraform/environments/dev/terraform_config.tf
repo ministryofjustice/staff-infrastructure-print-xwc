61df5546da2564540accfd31c83cea11e7c292e1
raghuvanshir <ravindra.raghuvanshi@justice.gov.uk>
2024-04-30 13:03:05 +0100

    state file changes

--- terraform/environments/dev/terraform_config.tf
+++ terraform/environments/dev/terraform_config.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.88.1"
+      version = "3.63.0"
     }
     random = {
       source  = "hashicorp/random"
       version = "3.1.0"
     }
   }
-  required_version = "=1.0.7"
+  required_version = "=1.5.7"
 
   backend "azurerm" {
     # Not required when run in pipeline, only required for command line testing
