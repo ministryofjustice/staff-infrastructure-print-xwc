ef4cfe1c3674f9c631651764a7ac116e82666a56
raghuvanshir <ravindra.raghuvanshi@justice.gov.uk>
2024-05-01 10:44:20 +0100

    updating azurerm version

--- terraform/environments/dev/terraform_config.tf
+++ terraform/environments/dev/terraform_config.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.63.0"
+      version = "3.101.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.1.0"
+      version = "3.6.1"
     }
   }
   required_version = "=1.5.7"
